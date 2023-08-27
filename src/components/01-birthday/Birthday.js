99e1cc22dd84b72dcc05f811534718ac86da6110
Yigit Er <mr.yigiter222@gmail.com>
2023-08-27 22:54:12 +0300

    v1

--- src/components/01-birthday/Birthday.js
+++ src/components/01-birthday/Birthday.js
@@ -9,7 +9,7 @@ const Birthday = () => {
     <main>
       <section className='container'>
         <h3>{people.length} birthdays today</h3>
-        <List people={people} />
+        <List peoples={people} />
         <button onClick={() => setPeople([])}>clear all</button>
       </section>
     </main>
