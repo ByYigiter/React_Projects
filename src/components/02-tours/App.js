e67e6a9f9f2ccd8f56fd638b9e6a195906db3500
Yigit Er <mr.yigiter222@gmail.com>
2023-08-28 21:54:59 +0300

    tours

--- src/components/02-tours/App.js
+++ src/components/02-tours/App.js
@@ -2,18 +2,17 @@ import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
 import NotLoading from './NotLoading'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tours-project'
 
 function App() {
   const [loading, setLoading] = useState(true)
   const [tours, setTours] = useState([])
 
-  // const removeTour = (id) => {
-  //   const newTours = tours.filter((tour) => tour.id !== id)
-  //   setTours(newTours)
-  // }
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
 
 
 
@@ -34,8 +33,8 @@ function App() {
   }
 
   useEffect(() => {
-    fetchTours();  // () parantez oalrak kullnadığımız doğrudan cağır anlamındadır
-  },[])// [] dizi değiştiği zaman useeffect tetiklenecek
+    fetchTours();  // () parantez oalrak kullnadığımız doğrudan cağırır
+  },[])// [] kontrol eder. dizi değiştiği zaman useeffect tetiklenecek
 
   if (loading) {
     return (
@@ -44,22 +43,22 @@ function App() {
       </main>
     )
   }
-  // if (tours.length === 0) {
-  //   return (
-  //     <main>
-  //       <div className='title'>
-  //         <h2>no tours left</h2>
-  //         <button className='btn' onClick={() => fetchTours()}>
-  //           refresh
-  //         </button>
-  //       </div>
-  //     </main>
-  //   )
-  // }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    )
+  }
   return (
     <main>
-      {/* <Tours tours={tours} removeTour={removeTour} /> */}
-      < NotLoading />
+      <Tours tours={tours} removeTour={removeTour} />
+      {/* < NotLoading /> */}
     </main>
   )
 }
--- src/components/02-tours/Tour.js
+++ src/components/02-tours/Tour.js
@@ -10,12 +10,13 @@ const Tour = ({ id, image, info, name, price, removeTour }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>
+        <span>
           {readMore ? info : `${info.substring(0, 200)}...`}
-          <button onClick={() => setReadMore(!readMore)}>
+        </span>
+        <button onClick={() => setReadMore(!readMore)}>
             {readMore ? 'show less' : '  read more'}
           </button>
-        </p>
+
         <button className="delete-btn" onClick={() => removeTour(id)}>
           not interested
         </button>
