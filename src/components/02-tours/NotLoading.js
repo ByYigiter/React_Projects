bbad6494066e52c1e8595b2a0e225a06dbafb2bc
Yigit Er <mr.yigiter222@gmail.com>
2023-08-31 07:14:31 +0300

    v1

--- src/components/02-tours/NotLoading.js
+++ src/components/02-tours/NotLoading.js
@@ -1,14 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const NotLoading = () => {
-  return (
-    <div className='loading'>
-        <h1>
-            Yukleme Sayfasını atladı...
-        </h1>
+	return (
+		<div className="loading">
+			<h1>Yukleme Sayfasını atladı...</h1>
+		</div>
+	);
+};
 
-    </div>
-  )
-}
-
-export default NotLoading
+export default NotLoading;
