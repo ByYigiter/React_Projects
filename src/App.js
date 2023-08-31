80830aef158b88d181ea19d8f3667d551ded9735
Yigit Er <mr.yigiter222@gmail.com>
2023-08-31 18:22:39 +0300

    Sepet v1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 // import Birthday from './components/01-birthday/Birthday'
 import Home from './components/02-tours/Home'
+import Header from './components/03-sepet-uygulama/Header'
+import SepetApp from './components/03-sepet-uygulama/SepetApp'
 
 const App = () => {
   return (
     <div>
       {/* <Birthday/> */}
-      <Home/>
+      {/* <Home/> */}
+      <SepetApp/>
     </div>
   )
 }
---
+++ src/components/03-sepet-uygulama/SepetApp.js
@@ -0,0 +1,41 @@
+import '../03-sepet-uygulama/SepetHome.css';
+import {useState, useEffect} from 'react'
+import Header from '../03-sepet-uygulama/Header';
+import Product from '../03-sepet-uygulama/Product';
+import Basket from '../03-sepet-uygulama/Basket';
+import products from './products.json'
+
+function App() {
+
+	const [money, setMoney] = useState(10000)
+	const [basket, setBasket] = useState([])
+	const [total, setTotal] = useState(0)
+
+	const resetBasket = () => {
+		setBasket([])
+	}
+
+	useEffect(() => {
+		setTotal(
+			basket.reduce((acc, item) => {
+				return acc + (item.amount * (products.find(product => product.id === item.id).price))
+			}, 0),
+		)
+	}, [basket])
+
+	return (
+		<>
+			<Header total={total} money={money}/>
+			<div className="container products">
+				{products.map(product => (
+					<Product key={product.id} total={total} money={money} basket={basket} setBasket={setBasket} product={product}/>
+				))}
+			</div>
+			{total > 0 && (
+				<Basket resetBasket={resetBasket} products={products} total={total} basket={basket} />
+			)}
+		</>
+	);
+}
+
+export default App;
