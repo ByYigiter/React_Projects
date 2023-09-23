3d0ea9e10f08b8a39a09627970bd11a4336c9dcf
Yigit Er <mr.yigiter222@gmail.com>
2023-09-23 12:51:53 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react'
 // import Birthday from './components/01-birthday/Birthday'
-import Home from './components/02-tours/Home'
-import Header from './components/03-sepet-uygulama/Header'
+// import Home from './components/02-tours/Home'
+// import Header from './components/03-sepet-uygulama/Header'
 import SepetApp from './components/03-sepet-uygulama/SepetApp'
-import AutoCompleteApp from './components/04-autoComplete/AutoCompleteApp'
-import BarcodeApp from './components/05-barcod/BarcodeApp'
+// import AutoCompleteApp from './components/04-autoComplete/AutoCompleteApp'
+// import BarcodeApp from './components/05-barcod/BarcodeApp'
 
 const App = () => {
   return (
     <div>
       {/* <Birthday/> */}
       {/* <Home/> */}
-      {/* <SepetApp/> */}
+      <SepetApp/>
       {/* <AutoCompleteApp/> */}
-      <BarcodeApp/>
+      {/* <BarcodeApp/> */}
     </div>
   )
 }
