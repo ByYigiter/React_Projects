acab1fa816ce5b4cb0dd5602303fbf7469019d71
Yigit Er <mr.yigiter222@gmail.com>
2023-10-23 21:56:45 +0300

    v1

--- src/components/04-autoComplete/AutoCompleteApp.js
+++ src/components/04-autoComplete/AutoCompleteApp.js
@@ -1,27 +1,25 @@
-import {useEffect, useRef, useState} from 'react';
-import ContentLoader from "react-content-loader"
-import './AutoComplete.css'
-
+import { useEffect, useRef, useState } from "react";
+import ContentLoader from "react-content-loader";
+import "./AutoComplete.css";
 
 const data = [
 	{
 		id: 1,
-		title: "test 1"
+		title: "test 1",
 	},
 	{
 		id: 2,
-		title: "Test 2"
+		title: "Test 2",
 	},
 	{
 		id: 3,
-		title: "deneme 1"
+		title: "deneme 1",
 	},
 	{
 		id: 4,
-		title: "Deneme 2"
-	}
-]
- 
+		title: "Deneme 2",
+	},
+];
 
 const AutocompleteLoader = () => (
 	<ContentLoader
@@ -37,45 +35,44 @@ const AutocompleteLoader = () => (
 		<rect x="96" y="20" rx="0" ry="0" width="169" height="8" />
 		<rect x="96" y="35" rx="0" ry="0" width="92" height="6" />
 	</ContentLoader>
-)
+);
 
 function AutoCompleteApp() {
-	const [search, setSearch] = useState('')
-	const [result, setResult] = useState(false)
-	const [loading, setLoading] = useState(false)
-	const searchRef = useRef()
+	const [search, setSearch] = useState("");
+	const [result, setResult] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const searchRef = useRef();
 
-	const isTyping = search.replace(/\s+/, '').length > 0;
+	const isTyping = search.replace(/\s+/, "").length > 0;
 
 	useEffect(() => {
-		document.addEventListener('mousedown', handleClickOutside)
+		document.addEventListener("mousedown", handleClickOutside);
 		return () => {
-			document.removeEventListener('mousedown', handleClickOutside)
-		}
-	}, [])
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, []);
 
 	const handleClickOutside = (e) => {
-		if (searchRef.current && !searchRef.current.contains(e.target)){
-			setSearch('')
+		if (searchRef.current && !searchRef.current.contains(e.target)) {
+			setSearch("");
 		}
-	}
+	};
 
 	const getResultItem = (item) => {
 		// console.log(item)
 		window.location.href = item.url;
-	}
+	};
 
 	useEffect(() => {
-		console.log(loading)
-	}, [loading])
+		console.log(loading);
+	}, [loading]);
 
 	useEffect(() => {
-
-		if (isTyping){
-			
-			const filteredResult = data.filter(item => item.title.toLowerCase().includes(search.toLowerCase()))
-			setResult(filteredResult.length > 0 ? filteredResult : false)
-		
+		if (isTyping) {
+			const filteredResult = data.filter((item) =>
+				item.title.toLowerCase().includes(search.toLowerCase())
+			);
+			setResult(filteredResult.length > 0 ? filteredResult : false);
 
 			// setLoading(true)
 			// const getData = setTimeout(() => {
@@ -91,28 +88,38 @@ function AutoCompleteApp() {
 			// 	clearTimeout(getData)
 			// 	setLoading(false)
 			// }
-
 		} else {
-			setResult(false)
+			setResult(false);
 		}
-
-	}, [search])
+	}, [search]);
 
 	return (
 		<>
 			<div className="search" ref={searchRef}>
-				<input type="text" value={search} className={isTyping ? 'typing' : null} placeholder="Bir şeyler ara.." onChange={(e) => setSearch(e.target.value)}/>
+				<input
+					type="text"
+					value={search}
+					className={isTyping ? "typing" : null}
+					placeholder="Bir şeyler ara.."
+					onChange={(e) => setSearch(e.target.value)}
+				/>
 				{isTyping && (
 					<div className="search-result">
-						{result && loading === false && result.map(item => (
-							<div onClick={() => getResultItem(item)} key={item.id} className="search-result-item">
-								<img src={item.image} alt=""/>
-								<div>
-									<div className="test">{item.title}</div>
-									<div className="date">{item.date}</div>
+						{result &&
+							loading === false &&
+							result.map((item) => (
+								<div
+									onClick={() => getResultItem(item)}
+									key={item.id}
+									className="search-result-item"
+								>
+									<img src={item.image} alt="" />
+									<div>
+										<div className="test">{item.title}</div>
+										<div className="date">{item.date}</div>
+									</div>
 								</div>
-							</div>
-						))}
+							))}
 						{loading && new Array(3).fill().map(() => <AutocompleteLoader />)}
 						{!result && loading === false && (
 							<div className="result-not-found">
