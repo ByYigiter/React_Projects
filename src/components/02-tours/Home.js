d3b21d37ca79758518f7b3517eabe31f3efca549
Yigit Er <mr.yigiter222@gmail.com>
2023-10-24 08:21:37 +0300

    v1

--- src/components/02-tours/Home.js
+++ src/components/02-tours/Home.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import App from './App'
-import './index.css'
+import React from "react";
+import App from "./App";
+import "./index.css";
 
 const Home = () => {
-  return (
-    <div>
-        <App/>
-    </div>
-  )
-}
+	return (
+		<div>
+			<App />
+		</div>
+	);
+};
 
-export default Home
+export default Home;
